c0732cfcba813cfe107ce101c637c4146fb5a8fd
William Thorsen <william@thorsen.dev>
2024-08-16 15:13:28 -0700

    fix | Config without include is not updated
    Fixed the issue where `git-recon.sh --install` failed to update a `.gitconfig` file that did not already have an `include` section.

--- bin/git-recon.sh
+++ bin/git-recon.sh
@@ -5,17 +5,20 @@ set -e
 BREW_PREFIX=$(brew --prefix)
 GITCONFIG_PATH="$HOME/.gitconfig"
 GIT_RECON_CONFIG_PATH="$BREW_PREFIX/etc/gitconfig.d/git-recon.gitconfig"
-INCLUDE_SECTION="[include]\n\tpath = $GIT_RECON_CONFIG_PATH\n"
+INCLUDE_SECTION="[include]\n    path = $GIT_RECON_CONFIG_PATH\n"
 
 install_config() {
   if [ -f "$GITCONFIG_PATH" ]; then
     if grep -q "$GIT_RECON_CONFIG_PATH" "$GITCONFIG_PATH"; then
       echo "Git-Recon configuration already included in $GITCONFIG_PATH"
-    else
+    elif grep -q "^\[include\]" "$GITCONFIG_PATH"; then
       echo "Adding Git-Recon configuration to $GITCONFIG_PATH..."
       sed -i '' "/\[include\]/ a\\
-      \ \ \ \ path = $GIT_RECON_CONFIG_PATH
-      " "$GITCONFIG_PATH" || echo -e "$INCLUDE_SECTION" >> "$GITCONFIG_PATH"
+$(printf '    ')path = $GIT_RECON_CONFIG_PATH
+" "$GITCONFIG_PATH"
+    else
+      echo "Adding new [include] section with Git-Recon configuration to $GITCONFIG_PATH..."
+      printf "\n[include]\n    path = %s\n" "$GIT_RECON_CONFIG_PATH" >> "$GITCONFIG_PATH"
     fi
   else
     echo "Creating $GITCONFIG_PATH with Git-Recon configuration"
@@ -23,12 +26,28 @@ install_config() {
   fi
 }
 
-case "$1" in
-  --install)
-    install_config
-    ;;
-  *)
-    echo "Usage: git-recon --install"
-    exit 1
-    ;;
-esac
+usage() {
+  echo "Usage: git-recon --install [--config-path=/path/to/.gitconfig]"
+  exit 1
+}
+
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+    --install)
+      ACTION="install"
+      ;;
+    --config-path=*)
+      GITCONFIG_PATH="${1#*=}"
+      ;;
+    *)
+      usage
+      ;;
+  esac
+  shift
+done
+
+if [ "$ACTION" = "install" ]; then
+  install_config
+else
+  usage
+fi
